17bbec221fb97594be1763d50cba652e57a44deb
Avadakedvr <Avadakedvr@users.noreply.github.com>
2020-01-11 22:34:48 +0000

    updating db

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const image = require('./controllers/image')
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-amorphous-10080',
     user : 'postgres',
-    password : 'Hack4life',
+    password : '',
     database : 'facerecognition'
   }
 });
