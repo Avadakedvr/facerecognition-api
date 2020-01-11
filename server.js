92099f1a0dea7585e6107d58fb920c60b0008718
Avadakedvr <Avadakedvr@users.noreply.github.com>
2020-01-11 23:10:27 +0000

    fixing cors

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ const db = require('knex')({
 const app = express(); //initiating app
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //END POINTS
 app.get('/', (request, response) => {response.send('It is working!'); })
